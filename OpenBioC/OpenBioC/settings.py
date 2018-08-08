db1cf0c7e1e1afb31f867685a2fd28672f92b6e5
kantale <alexandros.kanterakis@gmail.com>
2018-08-08 14:05:58 +0300

    init commit of OpenBioC

--- OpenBioC/OpenBioC/settings.py
+++ OpenBioC/OpenBioC/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!!!!!!!!!!!!!!!thisisnotasecretkey!!!!!!!!!!!!!!!!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'app',
 ]
 
 MIDDLEWARE = [
