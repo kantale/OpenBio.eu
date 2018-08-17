b0784ba95e38945fb7a9d11d3061dc1cfb8dcb80
kantale <alexandros.kanterakis@gmail.com>
2018-08-17 17:33:59 +0300

    added signup form

--- OpenBioC/app/static/app/OBC_ctrl.js
+++ OpenBioC/app/static/app/OBC_ctrl.js
@@ -1,18 +1,66 @@
 
+/*
+inner_: functions / variables not connected with the UI
+show_ : shows / hide divs
+*/
+
 app.controller("OBC_ctrl", function($scope) {
     $scope.init = function() {
         $scope.inner_hide_all_navbar();
     };
 
     /*
-    *
+    * Helper function that perform ajax calls
+    * success_view: what to do if data were correct and call was successful
+    * fail_view: What to do if call was succesful but data where incorrect
+    * fail_ajax: what to do if ajax call was incorrect
     */
-    $scope.inner_hide_all_navbar = function() {
-    	$scope.show_login = false;
+    $scope.ajax = function(url, data, success_view, fail_view, fail_ajax) {
+        // URL should always end with '/'
+
+        console.log('Before Ajax, data:');
+        console.log(data);
+
+        data.csrftoken = CSRF_TOKEN;
+
+        $http({
+            headers: {
+                "Content-Type": 'application/json',
+                "Access-Control-Allow-Origin": "*", // TODO : REMOVE THIS!
+                //"X-CSRFToken" : getCookie('csrftoken'),
+                "X-CSRFToken" : window.CSRF_TOKEN,
+            },
+            method : "POST",
+            url : url,
+            data : data
+        }).then(function mySucces(response) {
+            // $scope.myWelcome = response.data;
+            // alert(JSON.stringify(response));
+            if (response['data']['success']) {
+                console.log('AJAX SUCCESS:');
+                console.log(response['data']['success']);
+                success_view(response['data']);
+            }
+            else {
+                console.log('AJAX ERROR:');
+                fail_view(response['data']);
+            }
+            
+        }, function myError(response) {
+            fail_ajax(response.statusText);
+        });
     };
 
     /*
-    *
+    * Hide everything 
+    */
+    $scope.inner_hide_all_navbar = function() {
+    	$scope.show_login = false;
+        $scope.show_signup = false;
+    };
+
+    /*
+    * navbar --> Login --> Pressed
     */
     $scope.navbar_login_pressed = function() {
     	$scope.inner_hide_all_navbar();
@@ -21,10 +69,25 @@ app.controller("OBC_ctrl", function($scope) {
     };
 
     /*
-    *
+    * Navbar --> Signup --> pressed 
     */
     $scope.navbar_signup_pressed = function() {
     	$scope.inner_hide_all_navbar();
+
+        $scope.show_signup = true;
+    };
+
+    /*
+    * Navbar --> Signup --> Signup (button) --> Pressed
+    */
+    $scope.signup_signup_pressed = function() {
+        // signup_username
+        // signup_password
+        // signup_confirm_password
+        // signup_email 
+
+        
+
     };
 }); 
 
