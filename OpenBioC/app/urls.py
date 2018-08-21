36582a7464209d92d675fd4836361574a320a25a
Kanterakis Alexandros <kanterakis@staging.openbio.eu>
2018-08-21 22:32:20 +0300

    user profile logic

--- OpenBioC/app/urls.py
+++ OpenBioC/app/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
 	path('logout/', views.logout), # Logout a user
 	path('reset_password_email/', views.reset_password_email), # Reset password email
 	path('password_reset/', views.password_reset), # Reset user password 
+	path('user_data_get/', views.user_data_get), # Get data from a LOGGED IN user
+	path('user_data_set/', views.user_data_set), # Set data for a LOGGED IN user
 ]
