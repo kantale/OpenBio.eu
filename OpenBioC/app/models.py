45d1307d861ca5b12bc9967d3a936ef7626c36f9
Kanterakis Alexandros <kanterakis@staging.openbio.eu>
2018-08-21 16:45:40 +0300

    reset password logic

--- OpenBioC/app/models.py
+++ OpenBioC/app/models.py
@@ -5,12 +5,14 @@ from django.contrib.auth.models import User
 After making changes here run:
 python manage.py makemigrations
 python manage.py migrate;
+
+Important: When adding new fields, declare a default value (null=True ?)
 '''
 
 class OBC_user(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE) # Basically we will never delete users ??
     email_validated = models.BooleanField() # Is this user's email validated?
     email_validation_token = models.CharField(max_length=32) # This is a uuid4
-    password_reset_token = models.CharField(max_length=32) # A token to reset the password
-    password_reset_timestamp = models.DateTimeField() # When the request to update the password was done 
+    password_reset_token = models.CharField(max_length=32, null=True) # A token to reset the password
+    password_reset_timestamp = models.DateTimeField(null=True) # When the request to update the password was done 
 
--- OpenBioC/app/urls.py
+++ OpenBioC/app/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
 	path('login/', views.login), # Login a user
 	path('logout/', views.logout), # Logout a user
 	path('reset_password_email/', views.reset_password_email), # Reset password email
+	path('password_reset/', views.password_reset), # Reset user password 
 ]
