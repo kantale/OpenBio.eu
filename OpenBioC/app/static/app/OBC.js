daedc50860d73a69a62b7d615d13c8fe8ddcc90e
kantale <alexandros.kanterakis@gmail.com>
2018-09-10 17:50:32 +0300

    search jstree logic

--- OpenBioC/app/static/app/OBC.js
+++ OpenBioC/app/static/app/OBC.js
@@ -11,9 +11,34 @@ tool_validation_editor.session.setMode("ace/mode/sh");
 // https://github.com/ezraroi/ngJsTree/issues/20
 // We have to register the event on the document.. 
 $(document).on('dnd_stop.vakata', function (e, data) {
-    console.log('Stopped');
+	var target = $(data.event.target);
+	var this_id = data.data.nodes[0]; // plink/1.9/3/1"
 
-    angular.element($('#angular_div')).scope().$apply(function(){
-				angular.element($('#angular_div')).scope().addNewNode_2();
-	});
+	var this_id_array = this_id.split('/'); //[ "plink", "1.9", "3", "1" ]
+
+	//Do nothing 
+
+//    angular.element($('#angular_div')).scope().$apply(function(){
+//				angular.element($('#angular_div')).scope().addNewNode_2();
+//	});
+});
+
+//JSTree item moves
+$(document).on('dnd_move.vakata', function (e, data) {
+	var target = $(data.event.target);
+	var this_id = data.data.nodes[0]; // plink/1.9/3/1"
+
+	var this_id_array = this_id.split('/'); //[ "plink", "1.9", "3", "1" ]
+
+
+	if (this_id_array[3] == '1') { // This is an item from tools_search tree
+		if (target.closest('#tools_dep_jstree_id').length) {
+			data.helper.find('.jstree-icon').removeClass('jstree-er').addClass('jstree-ok');
+		}
+		else {
+			data.helper.find('.jstree-icon').removeClass('jstree-ok').addClass('jstree-er');
+		}
+	}
+
+
 });
