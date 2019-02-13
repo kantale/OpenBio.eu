bb32c0a03bae290231595acd5711c05f6994e5a4
kantale <alexandros.kanterakis@gmail.com>
2019-02-13 14:43:10 +0200

    merge code from galateia

--- OpenBioC/app/static/app/OBC_galateia.js
+++ OpenBioC/app/static/app/OBC_galateia.js
@@ -427,8 +427,6 @@ window.onload = function () {
 	 		window.buildTree([]); // [] means empty workflow
 	 	}
 	 	window.initTree();
-	 	
-	} // if (true)
 
 		//clear function
 		window.clear=function() {
@@ -455,6 +453,10 @@ window.onload = function () {
 		}
 
 		function isIE() { return ((navigator.appName == 'Microsoft Internet Explorer') || ((navigator.appName == 'Netscape') && (new RegExp("Trident/.*rv:([0-9]{1,}[\.0-9]{0,})").exec(navigator.userAgent) != null))); }
+
+	 	
+	} // if (true)
+
 		
 	// END OF GALATEIA's code
 
