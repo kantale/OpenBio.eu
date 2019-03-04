1d733ec42125964d0b69832db11224d76a285dd8
kantale <alexandros.kanterakis@gmail.com>
2019-03-04 16:27:23 +0200

    slider->cy.resize(), example autocomplete

--- OpenBioC/app/static/app/OBC.js
+++ OpenBioC/app/static/app/OBC.js
@@ -1,4 +1,6 @@
 
+var lang = ace.require("ace/ext/language_tools");
+
 var tool_installation_editor = ace.edit("tool_installation_editor");
 tool_installation_editor.setTheme("ace/theme/textmate");
 tool_installation_editor.session.setMode("ace/mode/sh");
@@ -10,6 +12,39 @@ tool_validation_editor.session.setMode("ace/mode/sh");
 var workflow_step_editor = ace.edit("workflow_step_editor");
 workflow_step_editor.setTheme("ace/theme/textmate");
 workflow_step_editor.session.setMode("ace/mode/sh");
+
+workflow_step_editor.setOptions({
+    enableBasicAutocompletion: true,
+    enableSnippets: true,
+    enableLiveAutocompletion: true
+});
+
+var compl = {
+  identifierRegexps: [/[a-zA-Z_0-9\.\$\-\u00A2-\uFFFF]/],
+  getCompletions: function (editor, session, pos, prefix, callback) {
+    //alert(prefix);
+
+    callback(null, [
+    {
+    	 caption: 'ABCDEFGHI',
+    	 value: 'ABCDEFGHI',
+    	 meta: 'TOOL'
+    },
+    {
+    	 caption: 'KLM',
+    	 value: 'NOP',
+    	 meta: 'STEP'
+
+    }
+    ]);
+    //return;
+  }
+}
+
+
+// https://stackoverflow.com/questions/28920998/custom-autocompleter-and-periods
+lang.setCompleters();
+lang.addCompleter(compl);
 
 /*
 * It is important to register these events AFTER load so that they are triggered after custom JSTREE functions
