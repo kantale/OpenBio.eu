00309cb67b1d28bb71a2d9f164e884e923fffcd5
Manos <tp4002@edu.teicrete.gr>
2019-04-04 01:31:59 +0300

    fixed some errors (Dockerfile_name,content_template) #35

--- ExecutionEnvironment/client.py
+++ ExecutionEnvironment/client.py
@@ -19,8 +19,8 @@ wget http://zzz.bwh.harvard.edu/plink/dist/plink-1.07-x86_64.zip
 unzip plink-1.07-x86_64.zip
 
 cd plink-1.07-x86_64
-#./plink --noweb --file test
-./plink --noweb
+./plink --noweb --file test
+#./plink --noweb
 		"""
 	}
 
