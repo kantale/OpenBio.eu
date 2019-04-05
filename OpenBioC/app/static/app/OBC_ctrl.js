05939b7369b64c2b31b17c193f8cd926c11ba1ef
kantale <alexandros.kanterakis@gmail.com>
2019-04-05 14:36:47 +0300

    delete WF + delete inputs/outpusi #29

--- OpenBioC/app/static/app/OBC_ctrl.js
+++ OpenBioC/app/static/app/OBC_ctrl.js
@@ -1744,7 +1744,13 @@ app.controller("OBC_ctrl", function($scope, $http, $filter, $timeout, $log) {
     */
     $scope.workflow_cytoscape_delete_node = function(node_id) {
 
-        /* Remove thre successors of a node*/
+        //Cannot edit a saved worfklow
+        if (!$scope.workflows_info_editable) {
+            $scope.toast('Cannot edit a saved workflow. Fork it to make edits.', 'error');
+            return;
+        }
+
+        /* Remove the successors of a node */
         function remove_successors(node) {
             node.successors().targets().forEach(function (element) {
                 cy.remove(element);                  
@@ -1795,6 +1801,15 @@ app.controller("OBC_ctrl", function($scope, $http, $filter, $timeout, $log) {
                node.remove();
             }
         }
+        else if (data.type == 'workflow') {
+            if (!data.belongto) {
+                $scope.toast('Cannot remove the root workflow', 'error');
+            }
+            else {
+                remove_successors(node);
+                node.remove();
+            }
+        }
 
     };
 
@@ -1836,6 +1851,10 @@ app.controller("OBC_ctrl", function($scope, $http, $filter, $timeout, $log) {
     */
     $scope.workflow_step_remove_input_output = function(index) {
         $scope.workflow_input_outputs.splice(index, 1);
+        //workflow_input_outputs should never be empty
+        if (!$scope.workflow_input_outputs.length) {
+            $scope.workflow_step_add_input_output();
+        }
     };
 
     /*
