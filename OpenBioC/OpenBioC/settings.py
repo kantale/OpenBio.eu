f5a9554efbb695ccb5683ad7df4ff934021b10fc
Alexandros Kanterakis <alexandros.kanterakis@gmail.com>
2019-04-10 11:44:14 +0300

    script to install all preseq css+js

--- OpenBioC/OpenBioC/settings.py
+++ OpenBioC/OpenBioC/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!!!!!!!!!!!!!!!thisisnotasecretkey!!!!!!!!!!!!!!!!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', 'staging.openbio.eu', '127.0.0.1']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', 'staging.openbio.eu', '127.0.0.1', '139.91.190.79']
 
 
 # Application definition
