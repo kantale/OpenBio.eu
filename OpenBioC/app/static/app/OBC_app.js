93fda1012093e03039b9c79f130610e8e6797d9d
kantale <alexandros.kanterakis@gmail.com>
2019-06-19 15:57:39 +0300

    fix broken OBC_app.js

--- OpenBioC/app/static/app/OBC_app.js
+++ OpenBioC/app/static/app/OBC_app.js
@@ -1,1 +1,1 @@
-var app = angular.module("OBC_app", ['ngJsTree', 'ui.select', 'ngSanitize']).config(); // 'ui.tree' , 'treeControl' 
+var app = angular.module("OBC_app", ['ngJsTree', 'ui.select', 'ngSanitize']); // 'ui.tree' , 'treeControl' 
