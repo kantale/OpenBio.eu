754bc942a85923270861257a79a37f662af00e00
kantale <alexandros.kanterakis@gmail.com>
2019-12-29 12:42:35 +0200

    dnd on tests #148

--- OpenBioC/app/static/app/tests.js
+++ OpenBioC/app/static/app/tests.js
@@ -1,63 +1,121 @@
 
 /*
-* Run with window.test.test() 
+* Testing suite for OpenBio.eu
+* Run with test.test(prefix); i.e. test.test('test1')
 */
 (function (){
 	var ret = {};
 
 	/*
-	* https://ghostinspector.com/blog/simulate-drag-and-drop-javascript-casperjs/ 
+	* https://github.com/kemokid/scripting-sortable/blob/master/script_sortable_dnd_more_general.js
 	*/
-	var triggerDragAndDrop = function (selectorDrag, selectorDrop) {
-
-	  // function for triggering mouse events
-	  var fireMouseEvent = function (type, elem, centerX, centerY) {
-	    var evt = document.createEvent('MouseEvents');
-	    evt.initMouseEvent(type, true, true, window, 1, 1, 1, centerX, centerY, false, false, false, false, 0, elem);
-	    elem.dispatchEvent(evt);
-	  };
+	var triggerDragAndDrop2 = function (selectorDrag, selectorDrop, callback) {
+	  var DELAY_INTERVAL_MS = 10;
+	  var MAX_TRIES = 10;
+	  var dragStartEvent;
 
 	  // fetch target elements
 	  var elemDrag = document.querySelector(selectorDrag);
 	  var elemDrop = document.querySelector(selectorDrop);
-	  if (!elemDrag || !elemDrop) return false;
 
-	  // calculate positions
-	  var pos = elemDrag.getBoundingClientRect();
-	  var center1X = Math.floor((pos.left + pos.right) / 2);
-	  var center1Y = Math.floor((pos.top + pos.bottom) / 2);
-	  pos = elemDrop.getBoundingClientRect();
-	  var center2X = Math.floor((pos.left + pos.right) / 2);
-	  var center2Y = Math.floor((pos.top + pos.bottom) / 2);
-	  
-	  // mouse over dragged element and mousedown
-	  fireMouseEvent('mousemove', elemDrag, center1X, center1Y);
-	  fireMouseEvent('mouseenter', elemDrag, center1X, center1Y);
-	  fireMouseEvent('mouseover', elemDrag, center1X, center1Y);
-	  fireMouseEvent('mousedown', elemDrag, center1X, center1Y);
+	  if (!elemDrag || !elemDrop) {
+	    console.log("can't get elements");
+	    return false;
+	  }
 
-	  // start dragging process over to drop target
-	  fireMouseEvent('dragstart', elemDrag, center1X, center1Y);
-	  fireMouseEvent('drag', elemDrag, center1X, center1Y);
-	  fireMouseEvent('mousemove', elemDrag, center1X, center1Y);
-	  fireMouseEvent('drag', elemDrag, center2X, center2Y);
-	  fireMouseEvent('mousemove', elemDrop, center2X, center2Y);
-	  
+	  var startingDropRect = elemDrop.getBoundingClientRect();
+
+	  function rectsEqual(r1, r2) {
+	    return r1.top === r2.top && r1.right === r2.right && r1.bottom === r2.bottom && r1.left === r2.left;
+	  }
+
+	  // function for triggering mouse events
+	  function fireMouseEvent(type, elem, dataTransfer) {
+	    var evt = document.createEvent('MouseEvents');
+	    evt.initMouseEvent(type, true, true, window, 1, 1, 1, 0, 0, false, false, false, false, 0, elem);
+	    if (/^dr/i.test(type)) {
+	      evt.dataTransfer = dataTransfer || createNewDataTransfer();
+	    }
+
+	    elem.dispatchEvent(evt);
+	    return evt;
+	  };
+
+	  function createNewDataTransfer() {
+	    var data = {};
+	    return {
+	      clearData: function(key) {
+	        if (key === undefined) {
+	          data = {};
+	        } else {
+	          delete data[key];
+	        }
+	      },
+	      getData: function(key) {
+	        return data[key];
+	      },
+	      setData: function(key, value) {
+	        data[key] = value;
+	      },
+	      setDragImage: function() {},
+	      dropEffect: 'none',
+	      files: [],
+	      items: [],
+	      types: [],
+	      // also effectAllowed      
+	    }
+	  };
+
 	  // trigger dragging process on top of drop target
-	  fireMouseEvent('mouseenter', elemDrop, center2X, center2Y);
-	  fireMouseEvent('dragenter', elemDrop, center2X, center2Y);
-	  fireMouseEvent('mouseover', elemDrop, center2X, center2Y);
-	  fireMouseEvent('dragover', elemDrop, center2X, center2Y);
-	  
-	  // release dragged element on top of drop target
-	  fireMouseEvent('drop', elemDrop, center2X, center2Y);
-	  fireMouseEvent('dragend', elemDrag, center2X, center2Y);
-	  fireMouseEvent('mouseup', elemDrag, center2X, center2Y);
+	  // We sometimes need to do this multiple times due to the vagaries of
+	  // how Sortable manages the list re-arrangement
+	  var counter = 0;
+	  function dragover() {
+	    counter++;
+	    console.log('DRAGOVER #' + counter);
+
+	    var currentDropRect = elemDrop.getBoundingClientRect();
+	    if (rectsEqual(startingDropRect, currentDropRect) && counter < MAX_TRIES) {
+	      if (counter != 1) console.log("drop target rect hasn't changed, trying again");
+
+	      // mouseover / mouseout etc events not necessary
+	      // dragenter / dragleave events not necessary either
+	      fireMouseEvent('dragover', elemDrop, dragStartEvent.dataTransfer);
+
+	      setTimeout(dragover, DELAY_INTERVAL_MS);
+	    } else {
+	      if (rectsEqual(startingDropRect, currentDropRect)) {
+	        console.log("wasn't able to budge drop target after " + MAX_TRIES + " tries, aborting");
+	        fireMouseEvent('drop', elemDrop, dragStartEvent.dataTransfer);
+
+	        //$(selectorDrop).mouseup(); 
+	        if (callback) callback(false);
+	      } else {
+	        setTimeout(drop, DELAY_INTERVAL_MS);
+	      }
+	    }
+	  }
+
+	  function drop() {
+	    console.log('DROP');
+	    // release dragged element on top of drop target
+	    fireMouseEvent('drop', elemDrop, dragStartEvent.dataTransfer);
+	    fireMouseEvent('mouseup', elemDrop);    // not strictly necessary but I like the symmetry
+	    if (callback) callback(true);
+	  }
+
+	  // start dragging process
+	  console.log('DRAGSTART');
+	  fireMouseEvent('mousedown', elemDrag);
+	  dragStartEvent = fireMouseEvent('dragstart', elemDrag);
+
+	  // after a delay, do the first dragover; this will run up to MAX_TRIES times
+	  // (with a delay between each run) and finally run drop() with a delay:
+	  setTimeout(dragover, DELAY_INTERVAL_MS);
 
 	  return true;
 	};
-	// triggerDragAndDrop('#tools_search_jstree_id a', '#tools_dep_jstree_id')
-	// $('#tools_dep_jstree_id').mouseup() 
+
 
 	ret.click = function(id) {
 		$('#' + id).click()
@@ -65,23 +123,46 @@
 
 	ret.insert = function(id, text) {
 		$('#' + id).val(text).trigger('input');
+		setTimeout(function(){M.updateTextFields()}, 10);
 	};
 
 	var click_plus_search_tools_button = function() {
 		ret.click('toolsDataPlusBtn');
 	};
 
+	var click_plus_search_workflows_button = function() {
+		ret.click('workflowsPlusBtn');
+	};
+
 	var tools_set_name = function() {
 		ret.insert('generalName', ret.prefix)
 	};
 
+	var workflows_set_name = function() {
+		ret.insert('generalWorkflowName', ret.prefix)
+	};
+
 	var tools_set_version = function() {
 		ret.insert('generalVersion', '1');
-	}
+	};
 
 	var tools_set_description = function() {
 		ret.insert('generalDescription', 'description');
-	}
+	};
+
+	var tools_dnd_search_dep = function() {
+		triggerDragAndDrop2('#tools_search_jstree_id a', '#tools_dep_jstree_id');
+		setTimeout(function() { $('#tools_dep_jstree_id').mouseup(); }, 1000);
+	};
+
+	var workflows_dnd_tool_graph = function() {
+		triggerDragAndDrop2('#tools_search_jstree_id a', '#cywf');
+		setTimeout(function() { $('#cywf').mouseup(); }, 1000);
+	};
+
+	var workflows_set_description = function() {
+		ret.insert('workflowsGeneralDescription', 'description');
+	};
 
 	var tools_select_os_posix = function() {
 		//M.FormSelect.getInstance($('#tool_os_choices_select')).dropdown.open();
@@ -111,23 +192,35 @@
 
             $('#tool_os_choices_select').formSelect();
         });
-	}
+	};
 
 	var tools_close_all_accordions = function() {
 		M.Collapsible.getInstance($('#createToolDataAccordion')).close()
-	}
+	};
 
 	var tools_open_all_accordions = function() {
 		M.Collapsible.getInstance($('#createToolDataAccordion')).open();
-	}
+	};
 
 	var tool_save_button = function() {
 		ret.click('tool_save_button_id')
-	}
+	};
 
-	var test_2 = function() {
-		ret.insert('leftPanelSearch', 'draft');
-	}
+	var tool_cancel_button = function() {
+		ret.click('tool_cancel_button_id');
+	};
+
+	var remove_toast = function() {
+		$('.toast').remove();
+	};
+
+	var global_search = function() {
+		ret.insert('leftPanelSearch', ret.prefix);
+	};
+
+	var global_search_open_accordions = function() {
+		M.Collapsible.getInstance($('#searchResultsCollapsible')).open();
+	};
 
 	var build_chain = function(fns, i) {
 
@@ -152,10 +245,26 @@
 			tools_set_version,
 			tools_set_description,
 			tools_select_os_posix,
-			tool_save_button
+			// tools_dnd_search_dep,
+			tool_save_button,
+			tool_cancel_button,
+			remove_toast
 		];
 
-		return build_chain(actions, 0)
+		return build_chain(actions, 0);
+	};
+
+	var create_new_workflow = function() {
+		var actions = [
+			click_plus_search_workflows_button,
+			workflows_set_name,
+			workflows_set_description,
+			global_search,
+			global_search_open_accordions,
+			workflows_dnd_tool_graph
+		]
+
+		return build_chain(actions, 0);
 	};
 
 	ret.test = function(prefix) {
@@ -163,7 +272,8 @@
 		ret.prefix = prefix;
 
 		var actions = [
-			create_new_tool
+			//create_new_tool,
+			create_new_workflow
 		];
 
 		return build_chain(actions, 0);
