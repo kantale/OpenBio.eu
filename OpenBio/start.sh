4a5b9d2a0fb20a2db129f5f6849783cd62ca548c
Antony Chazapis <chazapis@ics.forth.gr>
2021-07-28 11:42:25 +0300

    Add initial migration

--- OpenBio/start.sh
+++ OpenBio/start.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-python manage.py migrate --run-syncdb && \
-python manage.py makemigrations && \
-python manage.py migrate
-
+python manage.py migrate --run-syncdb
 python manage.py runserver 0.0.0.0:8200
