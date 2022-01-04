ec90b7ac692d951b32f1b47297c4a8572947dc90
kantale <alexandros.kanterakis@gmail.com>
2022-01-04 18:02:44 +0200

    check for karvdash social at start

--- app/models.py
+++ app/models.py
@@ -58,49 +58,55 @@ class OBC_user(models.Model):
 
 @receiver(user_logged_in)
 def post_login(sender, user, request, **kwargs):
-    social = user.social_auth.get(provider='karvdash')
-    if social:
-        # Fetch Karvdash deployment settings from extra data returned with OIDC.
-        try:
-            registry_url = urlparse(social.extra_data['karvdash_registry_url'])
-            parameters = {'type': 'karvdash',
-                          'argo_url': social.extra_data['karvdash_argo_workflows_url'],
-                          'namespace': social.extra_data['karvdash_namespace'],
-                          'image_registry': '%s:%s' % (registry_url.hostname, registry_url.port),
-                          'image_cache_path': '/private/.imagecache',
-                          'work_path': '/private/openbio'}
-        except KeyError:
-            print('Missing required parameters from extra data returned with OIDC. Skipping setting up execution client...')
-            return
+    try:
+        social = user.social_auth.get(provider='karvdash')
+    except ObjectDoesNotExist:
+        return
 
-        # Get or create user.
+    if not social:
+        return 
+
+    # Fetch Karvdash deployment settings from extra data returned with OIDC.
+    try:
+        registry_url = urlparse(social.extra_data['karvdash_registry_url'])
+        parameters = {'type': 'karvdash',
+                      'argo_url': social.extra_data['karvdash_argo_workflows_url'],
+                      'namespace': social.extra_data['karvdash_namespace'],
+                      'image_registry': '%s:%s' % (registry_url.hostname, registry_url.port),
+                      'image_cache_path': '/private/.imagecache',
+                      'work_path': '/private/openbio'}
+    except KeyError:
+        print('Missing required parameters from extra data returned with OIDC. Skipping setting up execution client...')
+        return
+
+    # Get or create user.
+    try:
+        obc_user = OBC_user.objects.get(user=user)
+    except ObjectDoesNotExist as e:
+        obc_user = OBC_user(user=user, email_validated=True, email_validation_token=None)
+        obc_user.save()
+
+    # Add a default execution client.
+    profile_name = 'karvdash'
+    parameters_json = json.dumps(parameters)
+    try:
+        client = obc_user.clients.get(name=profile_name)
+    except ObjectDoesNotExist as e:
         try:
-            obc_user = OBC_user.objects.get(user=user)
+            client = ExecutionClient.objects.get(name=profile_name)
+            print('FETCHING')
         except ObjectDoesNotExist as e:
-            obc_user = OBC_user(user=user, email_validated=True, email_validation_token=None)
-            obc_user.save()
-
-        # Add a default execution client.
-        profile_name = 'karvdash'
-        parameters_json = json.dumps(parameters)
-        try:
-            client = obc_user.clients.get(name=profile_name)
-        except ObjectDoesNotExist as e:
-            try:
-                client = ExecutionClient.objects.get(name=profile_name)
-                print('FETCHING')
-            except ObjectDoesNotExist as e:
-                print('CREATING')
-                client = ExecutionClient(name=profile_name, parameters=parameters_json)
-                client.save()
-                obc_user.clients.add(client)
-
-        print('PARAMETERS OLD:', client.parameters)
-        print('PARAMETERS NEW:', client.parameters)
-        if client.parameters != parameters_json:
-            print('UPDATING')
-            client.parameters = parameters_json
+            print('CREATING')
+            client = ExecutionClient(name=profile_name, parameters=parameters_json)
             client.save()
+            obc_user.clients.add(client)
+
+    print('PARAMETERS OLD:', client.parameters)
+    print('PARAMETERS NEW:', client.parameters)
+    if client.parameters != parameters_json:
+        print('UPDATING')
+        client.parameters = parameters_json
+        client.save()
 
 class Keyword(models.Model):
     '''
