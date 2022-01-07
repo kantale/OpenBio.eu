86b50397a5fbe0a697570610b31200f34132d59e
Antony Chazapis <chazapis@ics.forth.gr>
2022-01-07 15:25:39 +0200

    Bump version

--- app/views.py
+++ app/views.py
@@ -99,7 +99,7 @@ import requests # Used in DOI resolution
 import mistune
 
 
-__version__ = '0.2.1b1'
+__version__ = '0.2.1b2'
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
